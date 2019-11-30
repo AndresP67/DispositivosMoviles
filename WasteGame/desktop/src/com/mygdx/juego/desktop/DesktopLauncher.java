77c7783de1e3659db9b079be1b33dcd0f45122d7
Familia <japineroso@poligran.edu.co>
2019-11-30 07:48:13 -0500

    Agregar WasteGame

---
+++ WasteGame/desktop/src/com/mygdx/juego/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.juego.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.Juego.Juego;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Juego(), config);
+	}
+}
